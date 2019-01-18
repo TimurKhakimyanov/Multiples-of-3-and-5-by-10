67b54dad35e743667ef7417c4f6fe8120b961eb0
TimurKhakimyanov <35830474+TimurKhakimyanov@users.noreply.github.com>
2019-01-18 23:16:49 +0600

    Add files via upload

---
+++ Multiples of 3 and 5 by 10.py
@@ -0,0 +1,26 @@
+#Код для выделения всех натуральных чисел меньше 10 которые кратны 3 и 5 и нахождения их  суммы.January 18,2019
+
+ten = int(10)
+three = int(3)
+five = int(5)
+simm = int(0)
+i = int(1)
+v = int(0)
+
+while i<ten:
+    v=i%three
+    if v == 0:
+        simm=i+simm
+        v=0
+    elif v>0:
+        pass
+    v=i%five
+    if v == 0:
+        simm=i+simm
+        v=0
+    elif v>0:
+        pass
+    i=i+1
+
+
+print(simm)
